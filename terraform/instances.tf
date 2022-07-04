72f8c5f4fff1a7aa2864ff2bab20870686957bf4
nikolasht1 <nikolasht@outlook.com>
2022-07-04 23:59:10 +0300

    setap_instans

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -14,9 +14,19 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "workshop" {
+  key_name   = "workshop"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name = aws_key_pair.workshop.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_server_sg.id
+  ]
 
   tags = {
     Name = "web"
