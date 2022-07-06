981226eb486174b3aeeb60be142fa850e2d33f2e
nikolasht1 <nikolasht@outlook.com>
2022-07-07 00:18:35 +0300

    fixed playbook

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -26,8 +26,8 @@ resource "aws_security_group_rule" "allow_ssh" {
 
 resource "aws_security_group_rule" "allow_http" {
   type              = "ingress"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 5000
+  to_port           = 5000
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.web_server_sg.id
